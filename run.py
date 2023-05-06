6672282477eca44f2a15217bb6bcbe5ad186cfd9
GeorgieEggleton <>
2023-05-06 12:45:36 +0000

    fixed bug after attempting to use global varible that didn't work, re-added arguments to functions

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import random
 
 guessed_letters = []
+word = ""
 
-def opening_screen():
+def opening_screen(word, guessed_letters):
     """
     Welcome text, Expains rules, waits for player to press start
     """
@@ -12,7 +13,7 @@ def opening_screen():
     game_start = input("Press 1 & enter to start \n")
     if game_start == "1":
         word = pickword()
-        play_game(word)
+        play_game(word, guessed_letters)
 
     if game_start == "2": 
         display_rules()   
@@ -31,45 +32,56 @@ def display_rules():
 
 
 def pickword():
-    wordlist = ["yacht", "boat", "mast"]
+    wordlist = ["yacht", "port", "boat", "mast"]
     word = random.choice(wordlist)
+    print(word)
     return word.upper()
+    
    
 
-def play_game(word):
-    
+
+
+def play_game(word, guessed_letters):
+
+    letter_pick_and_validate(word, guessed_letters)
+   
+"""
     blank_word = "_ " *len(word)   
     lives = 10
     print(blank_word + "\n")
     print(lives)
+"""
 
 
 
-
-def letter_pick_and_validate(guessed_letters):
+def letter_pick_and_validate(word, guessed_letters):
+    print("pick a letter" + word)
     current_letter = input("Pick a letter \n")   
     
     if current_letter.isalpha():
         print("checking against word")
-        check_letter(current_letter, guessed_letters)
+        check_letter(word, guessed_letters, current_letter)
     else:
         print("not a letter")
-        letter_pick_and_validate(guessed_letters)
+        letter_pick_and_validate()
         
 
 
-def check_letter(current_letter, guessed_letters):
+def check_letter(word, guessed_letters, current_letter):
+    print(word)
     if current_letter in guessed_letters:
         print("You already guessed that letter")
         letter_pick_and_validate()
        
     else:
-        #check_against_word() 
         guessed_letters.append(current_letter)
+        check_against_word(word, guessed_letters) 
 
 
 
-def check_against_word(guessed_letters, word):
+def check_against_word(word, guessed_letters):
+    print("im in function check_against_word")
+    print(word)
     display_word = ""
     for i in word:
         if i in guessed_letters:
@@ -79,16 +91,30 @@ def check_against_word(guessed_letters, word):
             display_word += "_ "
     print(display_word)
 
+
+
+
+
 """
-opening_screen()    
-letter_pick_and_validate(guessed_letters)
+    if "_ " not in word:
+        print("you win :-)")
+    else:
+       letter_pick_and_validate() 
 """
+#def lives_left():
+
+
+
+
+opening_screen(word, guessed_letters)    
+letter_pick_and_validate(word, guessed_letters)
 
 
 
 
 
-check_against_word(["a", "c"], "yacht")
+
+#check_against_word(["y","a", "c", "h", "t"], "yacht")
     
 
 
