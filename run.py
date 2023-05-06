fe816706bab946174a8d93304e644f5ad118226b
GeorgieEggleton <>
2023-05-06 09:59:27 +0000

    Add pick a letter and validate function

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import random
 
+
+
 def opening_screen():
     """
     Welcome text, Expains rules, waits for player to press start
@@ -10,7 +12,7 @@ def opening_screen():
     game_start = input("Press 1 & enter to start \n")
     if game_start == "1":
         word = pickword()
-        play_game()
+        play_game(word)
 
     if game_start == "2": 
         display_rules()   
@@ -30,6 +32,46 @@ def display_rules():
 
 def pickword():
     wordlist = ["yacht", "boat", "mast"]
+
     word = random.choice(wordlist)
+    
     return word.upper()
    
+
+
+
+def play_game(word):
+    
+    blank_word = "_ " *len(word)   
+    lives = 10
+    guessed_letters = []
+    print(blank_word + "\n")
+    
+    print(lives)
+
+
+
+
+def letter_pick_and_validate():
+    current_letter = input("Pick a letter \n")   
+
+    if current_letter.isalpha():
+        print("checking against word")
+
+    else:
+        print("not a letter")
+        letter_pick_and_validate()
+        
+
+
+
+opening_screen()
+letter_pick_and_validate()
+
+
+
+
+
+
+
+    
