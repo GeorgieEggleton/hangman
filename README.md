b5eb4bd77ce99c92e05eb3dd21ba959a08fbd290
GeorgieEggleton <>
2023-05-09 21:10:28 +0000

    Add pplay again function at end of game

--- README.md
+++ README.md
@@ -1,32 +1,90 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# NAUTICAL HANGMAN
 
-Welcome,
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **March 14, 2023**
+## Nautical hangman is a terminal based Python game in which the user needs to guess the nautical word in 8 lives or less before the man is hung. 
 
-## Reminders
+View the live site here - https://nautical-hangman.herokuapp.com/
 
-- Your code must be placed in the `run.py` file
-- Your dependencies must be placed in the `requirements.txt` file
-- Do not edit any of the other files or your code may not deploy properly
+![Welcome Graphic](/readme-images/welcome-graphic-1.png)
+![Welcome Graphic](/readme-images/welcome-graphic-2.png)
 
-## Creating the Heroku app
+### User Experience 
+-	The game should be engaging and entertaining. 
+-	It should be easy for the user to interact with despite being terminal based
+-	It should be informative, by displaying the definition of the word after the round to allow the user to learn the nautical terms. 
+-	It should be easy for the user to play multiple rounds of the game. 
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
 
-1. `heroku/python`
-2. `heroku/nodejs`
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+### Design flowchart
+In order to check my logic for the flow of the game I created the following flow chart. 
 
-Connect your GitHub repository and deploy as normal.
 
-## Constraints
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
 
----
 
-Happy coding!
+### Features
+
+#### Welcome page  
+The  welcome graphic features and Ascii art boat and bubble writing game title “Nautical Hangman” As we are developing a terminal based 
+game using Ascii art seemed a nice way to make the game more visually interesting. This took a few attempts to dispaly correctly on the mock terminal and in order to show both the image and writting without the user needing to scroll up a 2 second delay was added after the boat image loads before the writting appears. 
+
+![Welcome Graphic](/readme-images/welcome-graphics-together.jpg)
+
+#### Start Options
+The player is then presented with 2 options. Press 1 to play the game or press 2 to read the rules. If something other than 1 or 2 is pressed and error massage is shown and it re-prompts them to select 1 or 2. 
+
+
+#### Rules
+If the player presses 2 and enter the rules of the game will be displayed, followed by the option to start the game. 
+
+#### The Game
+The game gives the player 7 lives to guess the nautical word. The word will be selected at random from the “words” list. The program will then display underscores for the number of letters in the word. The program will prompt the user to guess a letter. If they guess a correct letter the _ will be replaced with the letters, if they guess incorrectly they loose a life and a limb will be added to the hangman. 
+
+After each guess the letters that the user has guessed will be added to the guessed letters list and displayed on screen to try and help the user guess the word and stop them re-guessing the same letter. If they were to re-guess a letter already tried they would not loose a life but a message will show to tell them they already tried that letter before re-prompting for another letter. 
+
+If the player does not guess the word before the man is hung the loosing message is displayed followed b#uy the complete word and its definition to help the player learn the nautical terms. 
+
+If the player does guess the word before the man is hung the winning message is displayed followed by the word and definition. 
+
+After the end of each round the game will ask the player if they would like to play again. If they press Y the game will be re-loaded with a new random word. If they press N the game will exit. 
+
+
+## Future Features
+- In future additions I would like to a google sheet API to save the users scores so they can return and try to beat their last score
+- I would like to add the option for the user to pick a difficulty level (based on the number of letters in the word), this means the game could be targetted at mulitiple different age groups easier. 
+
+## Testing
+I ran the code through the Code Insitute python validator. A number of errors were picked up, mostly mnor issues such as trailing white space, lines too long, or too many line breaks. I have corrected all of these and the code now passes the validator with no errors found. 
+
+![Welcome Graphic](/readme-images/validator_screenshot.png)
+
+### BUGS
+On deployment to the Heroku mock terminal I found that the welcome graphic was too wide and broke. I therefore had to update the graphic to be less than 80 characters wide to ensure it fitted. 
+I also needed to move the Graphics "Nautical" and "Hangman" on to two seperate lines to ensure they fitted into the mcok terminal. 
+
+On delpoyment I found that if the image and words loaded at the same time the image was pushed up in the terminal out of view unless the user scrolled up. I therefore decided to load the boat graphic first, give a 2 second delay, then load the writting to give a better aesthetic.  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Made a decision that the guessed letters should be in guessed order not aplbetical order
+Test letters in guessed letters array not in order
+Made game text sit along side hangman graphic
+
+
+
+Bugs- 
+Input needs to be uppercase
--- run.py
+++ run.py
@@ -1,55 +1,54 @@
 import random
-#import gspread
-#from google.oauth2.service_account import Credentials
 import os
-from graphics import welcome_graphic 
+from graphics import welcome_graphic
 from graphics import hangman
 import time
 
-"""
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file(creds.json)
-SCOPED_CREDS = CREDS.with_scopes(scope)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('words')
-""" 
-
-guessed_letters = [] 
+guessed_letters = []
 word = ""
 wordlist = ["YACHT", "PORT", "BOAT", "MAST", "BAGGYWRINKLE"]
-definition = ["a floating thing", "the left", "A small flaoting thing", "The stick", "something Owain doesn't know"]
+definition = [
+    "a floating thing",
+    "the left",
+    "A small flaoting thing",
+    "The stick",
+    "something Owain doesn't know"
+    ]
+
 
 def opening_screen(word, guessed_letters):
     """
-    Welcome text, Expains rules, waits for player to press start
+    Displays welcome graphic 1, waits 2 seconds displays welcome graphic 2.
+    and triggers game_start
     """
     print(welcome_graphic[0])
     time.sleep(2)
     print(welcome_graphic[1])
-
     print("welcome to sailing hangman \n")
     print("Guess the sailing word before the man is hung! \n")
     game_start(word, guessed_letters)
 
 
 def game_start(word, guessed_letters):
+    """
+    Displays options to start the game, display the rules or exit
+    """
     start = input("Press 1 & enter to start or 2 & enter to read the rules \n")
+    start = start.strip(" ")
     if start == "1":
         word = pickword()
         check_against_word(word, [])
-   
-    elif start == "2": 
-        display_rules(word, guessed_letters)   
-   
-    else:
-        print("You need to press 1 or 2 and 'Enter' to either start the game or display the rules")
-        game_start(word, guessed_letters)
 
+    elif start == "2":
+        display_rules(word, guessed_letters)
+
+    elif start == "3":
+        exit()
+
+    else:
+        print("""You need to press 1 or 2 and 'Enter' to either start the game
+ or display the rules \n""")
+        game_start(word, guessed_letters)
 
 
 def display_rules(word, guessed_letters):
@@ -60,87 +59,95 @@ def display_rules(word, guessed_letters):
     print("Try to guess the nautical word before the hangman is complete \n")
     print("Type a letter and press Enter \n")
     print("If the letter is in the word it will show \n")
-    print("If the letter is not in the word you loose a live and get closer to hanging the man\n")
+    print("""If the letter is not in the word you loose a live and get closer
+ to hanging the man\n""")
     print("GOOD LUCK!!\n")
     game_start(word, guessed_letters)
 
 
-
 def pickword():
-
+    """
+    Picks a word at random from the word list and capitlizes it
+    """
     word = random.choice(wordlist)
     print(word)
     return word.upper()
-    
-   
-
-
-
-
 
 
 def letter_pick_and_validate(word, guessed_letters):
-    
-    current_letter = input("Pick a letter \n")   
-    
+    """
+    Prompts the user to pick a letter
+    Checks if it is a letter
+    If not a letter returns Error message and asks them to try again
+    """
+    current_letter = input("Pick a letter \n")
+
     if current_letter.isalpha():
-        
         check_letter(word, guessed_letters, current_letter.upper())
     else:
         print("not a letter")
         letter_pick_and_validate(word, guessed_letters)
-        
 
 
 def check_letter(word, guessed_letters, current_letter):
-    
+    """
+    Takes the letter picked by the user and checks if its been used before
+    Adds the new letter to the guessed letters list
+    """
     if current_letter in guessed_letters:
         print("You already guessed that letter")
         letter_pick_and_validate(word, guessed_letters)
-       
+
     else:
         guessed_letters.append(current_letter)
-        check_against_word(word, guessed_letters) 
-
+        check_against_word(word, guessed_letters)
 
 
 def check_against_word(word, guessed_letters):
+    
+    """
+    Clears the terminal to keep game space clear and readable
+    Checks each letter of the word in turn to see if the guessed letter is there
+    Displays the letter in the correct place
+    If the word is complete displays winning message, word and definition
+    """
     os.system("clear")
     display_word = ""
     for i in word:
         if i in guessed_letters:
-            display_word += i +" "
- 
+            display_word += i + " "
         else:
-            display_word += "_ "
-            
+            display_word += "_ "  
     print(display_word)
     if "_" in display_word:
         lives_left(word, guessed_letters)
-    else:    
+    else:
         print("YOU WIN!!!")
         wordnum = wordlist.index(word)
         worddef = definition[wordnum]
         print(f"{word} - {worddef}")
-
-
-
-
+        time.sleep(1)
+        play_again_option(word, guessed_letters)
 
 
 def lives_left(word, guessed_letters):
-
+    """
+    Updates lives left and displays next hangman life if incorrect guess.
+    If lives left gets to 0 display loosing message, word and definition
+    Give player the option to try again
+    """
     lives_left = 7
     for i in guessed_letters:
         if i not in word:
             lives_left -= 1
-   
     if lives_left <= 0:
-        print("You Lose!") 
         print(hangman[7])
+        print("Oh no! You Lose!\n")
         wordnum = wordlist.index(word)
         worddef = definition[wordnum]
         print(f"{word} - {worddef}")
+        time.sleep(1)
+        play_again_option(word, guessed_letters)
     else:
         print(hangman[7 - lives_left])
         print("\n")
@@ -148,7 +155,23 @@ def lives_left(word, guessed_letters):
         print(f"Letters you've already tried = {guessed_letters}")
         letter_pick_and_validate(word, guessed_letters)     
 
-opening_screen(word, guessed_letters)    
+def play_again_option(word, guessed_letters):       
+    play_again = input("Would you like to play again? Y/N \n")
+    play_again = play_again.strip(" ")
+    play_again = play_again.capitalize()
+    if play_again == "Y":
+        opening_screen(word, guessed_letters)
+                
+    elif play_again == "N":
+        print("Thanks for playing, come back soon!")
+        exit()
+        
+    else:
+        print("Not an Option Pick Y or N")
+        play_again_option(word, guessed_letters)    
+        
+    
+    
+           
 
-
-
+opening_screen(word, guessed_letters)
